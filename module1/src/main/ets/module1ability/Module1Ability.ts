896b87786581069993a4daac444e136ef5a8aee6
xiaojiniz <347837667@qq.com>
2023-11-26 14:34:50 +0800

    Page 和 UIAbility 数据交流1

--- module1/src/main/ets/module1ability/Module1Ability.ts
+++ module1/src/main/ets/module1ability/Module1Ability.ts
@@ -2,26 +2,46 @@ import UIAbility from '@ohos.app.ability.UIAbility';
 import hilog from '@ohos.hilog';
 import window from '@ohos.window';
 
+let localStorage: LocalStorage = new LocalStorage(
+  {
+    "CommunicateBetweenPageAndUiAbility1_key": "",
+  }
+);
+
 export default class Module1Ability extends UIAbility {
+
   onCreate(want, launchParam) {
-    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
+    this.context.eventHub.on(
+      "CommunicateBetweenPageAndUiAbility1",
+      (data1) => {
+
+        localStorage.setOrCreate(
+          "CommunicateBetweenPageAndUiAbility1_key",
+          `${data1}, this is from UiAbility`,
+        )
+
+      }
+    )
   }
 
   onDestroy() {
-    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onDestroy');
+    this.context.eventHub.off(
+      "CommunicateBetweenPageAndUiAbility1",
+    )
   }
 
   onWindowStageCreate(windowStage: window.WindowStage) {
     // Main window is created, set main page for this ability
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
 
-    windowStage.loadContent('pages/Index1', (err, data) => {
+    windowStage.loadContent('pages/Index1', localStorage, (err, data) => {
       if (err.code) {
         hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
         return;
       }
       hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
     });
+
   }
 
   onWindowStageDestroy() {
